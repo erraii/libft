057801d5c8e66f0d3b247c453903c12f1658524d
erraii <eraycakiray@gmail.com>
2026-04-25 22:37:00 +0200

    Add files via upload

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,32 +6,34 @@
 /*   By: ecakiray <ecakiray@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 14:16:54 by ecakiray          #+#    #+#             */
-/*   Updated: 2026/04/25 22:18:51 by ecakiray         ###   ########.fr       */
+/*   Updated: 2026/04/25 22:33:28 by ecakiray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_size;
 	size_t	src_size;
+	size_t	w_size;
 
 	dst_size = 0;
 	while ((dst_size < size) && dst[dst_size])
 		dst_size++;
 	src_size = 0;
+	w_size = 0;
 	while (src[src_size])
 	{
 		if (dst_size + src_size + 1 < size)
+		{
 			dst[dst_size + src_size] = src[src_size];
+			w_size++;
+		}
 		src_size++;
 	}
-	if (dst_size + src_size < size)
-		dst[dst_size + src_size] = '\0';
+	if (dst_size < size)
+		dst[dst_size + w_size] = '\0';
 	return (dst_size + src_size);
 }
 // int main ()
