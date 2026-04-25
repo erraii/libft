13eb7e3bcd0eedb7b6cdcecec90ebef0ea3447ab
erraii <eraycakiray@gmail.com>
2026-04-25 22:20:59 +0200

    Add files via upload

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,14 @@
 /*   By: ecakiray <ecakiray@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 14:16:54 by ecakiray          #+#    #+#             */
-/*   Updated: 2026/04/25 15:15:27 by ecakiray         ###   ########.fr       */
+/*   Updated: 2026/04/25 22:18:51 by ecakiray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
@@ -23,11 +26,23 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	src_size = 0;
 	while (src[src_size])
 	{
-		if ((dst_size + src_size + 1) < size)
+		if (dst_size + src_size + 1 < size)
 			dst[dst_size + src_size] = src[src_size];
 		src_size++;
 	}
-	if (size > dst_size)
+	if (dst_size + src_size < size)
 		dst[dst_size + src_size] = '\0';
 	return (dst_size + src_size);
 }
+// int main ()
+// {
+// 	char	*dest;
+// 	if (!(dest = (char *)malloc(sizeof(*dest) * 15)))
+// 		return (0);
+// 	memset(dest, 0, 15);
+// 	memset(dest, 'r', 6);
+// 	dest[14] = 'a';
+// 	printf("%zu\n",ft_strlcat(dest, "lorem ipsum dolor sit amet", 15));
+// 	printf("%s",dest);
+// 	return(0);
+// }
